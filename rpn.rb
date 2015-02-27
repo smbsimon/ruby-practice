ddeb7e91df711bf342754fec6cb3ab0db0d7fe4d
Sara Simon <smbsimon@gmail.com>
2015-02-26 23:43:43 -0700

    Simple operations working

--- rpn.rb
+++ rpn.rb
@@ -1,5 +1,23 @@
+require 'pry'
+
 class RPNCalculator
+
   def evaluate(rpn)
-    
+    split_characters = rpn.split(" ")
+
+    a = split_characters[0].to_i
+    b = split_characters[1].to_i
+
+
+    if split_characters[2] == "+"
+      new_element = a + b
+    elsif split_characters[2] == "-"
+      new_element = a - b
+    else split_characters[2] == "*"
+      new_element = a * b
+    end
+
+    new_element
+
   end
 end
