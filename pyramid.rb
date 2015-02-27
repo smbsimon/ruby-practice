eff6adfe0017ced7bf78dcea798382079af4222b
Sara Simon <smbsimon@gmail.com>
2015-02-26 20:12:37 -0700

    Add functionality to find sodas for a given bonus

--- pyramid.rb
+++ pyramid.rb
@@ -1,63 +1,35 @@
 class Pyramid
-  attr_reader :level,
-              :bonus,
+  attr_reader :bonus,
               :price
 
-  def initialize
-    @level   = level
-    @bonus   = 50
-    @price   = 2
+  attr_accessor :level
+
+  def initialize(bonus, price)
+    @level   = 0
+    @bonus   = bonus
+    @price   = price
   end
 
-  def total_sodas_per_level(level)
+  def total_sodas_per_level
     (1..level).map { |i| i*i }
   end
 
-  def total_sodas_in_pyramid(level)
-    total_sodas_per_level(level).reduce(:+)
+  def total_sodas_in_pyramid
+    total_sodas_per_level.reduce(:+)
   end
 
-  def total_available_sodas(bonus, price)
+  def total_available_sodas
     bonus / price
   end
 
-  def total_levels_for_a_given_bonus(bonus, price)
-    
+  def total_levels_for_a_given_bonus
+    pyramid_sodas = 0
+
+    while pyramid_sodas <= total_available_sodas do
+      @level += 1
+      pyramid_sodas = total_sodas_in_pyramid
+    end
+
+    return level - 1
   end
 end
-
-
-
-
-#
-# 50 / 2 = 25 sodas.
-# 25 sodas are on level 5
-# but what i want is to find the level at which count does not exceed 25 sodas... which is level 3
-# i want to find the level at which i don't exceed total_available_sodas
-
-
-
-
-
-
-
-# Let's pretend your company just hired your friend from college and paid you a referral bonus.
-# Awesome! To celebrate, you're taking your team next door and using the referral bonus to buy,
-# and build, the largest three-dimensional soda can pyramid you can.
-# And then probably drink those sodas, because let's pretend it's Friday too.
-
-
-
-# level 1 = 1     for total of 1 sodas
-# level 2 = 4     for total of 5 sodas
-# level 3 = 9     for total of 14 sodas
-# level 4 = 16    for total of 30 sodas
-# level 5 = 25    for total of 55 sodas
-
-
-
-# Complete the sodamid function to return the number of complete levels of a
-# soda can pyramid you can make, given the parameters of:
-#
-# 1) your referral bonus, and
-# 2) the price of a soda can
--- pyramid_test.rb
+++ pyramid_test.rb
@@ -5,58 +5,27 @@ require_relative 'pyramid'
 class PyramidTest < Minitest::Test
 
   def test_it_returns_array_of_sodas_per_level
-    pyramid = Pyramid.new
-    assert_equal [1, 4, 9], pyramid.total_sodas_per_level(3)
+    pyramid = Pyramid.new(50, 2)
+    pyramid.level = 3
+    assert_equal [1, 4, 9], pyramid.total_sodas_per_level
   end
 
   def test_it_returns_total_number_of_sodas_for_given_number_of_levels
-    pyramid = Pyramid.new
-    assert_equal 30, pyramid.total_sodas_in_pyramid(4)
-    assert_equal 55, pyramid.total_sodas_in_pyramid(5)
+    pyramid = Pyramid.new(50, 2)
+    pyramid.level = 4
+    assert_equal 30, pyramid.total_sodas_in_pyramid
+    pyramid.level = 5
+    assert_equal 55, pyramid.total_sodas_in_pyramid
   end
 
   def test_it_returns_total_available_sodas_given_bonus_and_price
-    pyramid = Pyramid.new
-    assert_equal 25, pyramid.total_available_sodas(50, 2)
+    pyramid = Pyramid.new(50, 2)
+    assert_equal 25, pyramid.total_available_sodas
   end
 
   def test_it_returns_number_of_levels_for_a_given_bonus
-    pyramid = Pyramid.new
-    assert_equal 3, pyramid.total_levels_for_a_given_bonus(50, 2)
+    pyramid = Pyramid.new(50, 2)
+    assert_equal 3, pyramid.total_levels_for_a_given_bonus
   end
 
 end
-
-
-
-
-# 50 / 2 = 25 sodas.
-# 25 sodas are on level 5
-# but what i want is to find the level that does not exceed 25 sodas... which is level 3
-
-
-
-
-
-# level 1 = 1     for total of 1 sodas
-# level 2 = 4     for total of 5 sodas
-# level 3 = 9     for total of 14 sodas
-# level 4 = 16    for total of 30 sodas
-# level 5 = 25    for total of 55 sodas
-
-
-
-# Let's pretend your company just hired your friend from college and paid you a referral bonus.
-# Awesome! To celebrate, you're taking your team next door and using the referral bonus to buy,
-# and build, the largest three-dimensional soda can pyramid you can.
-# And then probably drink those sodas, because let's pretend it's Friday too.
-
-
-# A soda can pyramid will square the number of cans in each level - 1 can in the top level,
-# 4 in the second, 9 in the next, 16, 25...
-#
-# Complete the sodamid function to return the number of complete levels of a
-# soda can pyramid you can make, given the parameters of:
-#
-# 1) your referral bonus, and
-# 2) the price of a soda can
